8ed18dfc8349e1ba4240719b17d6daf72bed67c6
Axel.Razafimahefa <axel.raza75@hotmail.fr>
2020-03-18 23:28:45 +0100

    Load words from api call

--- app/src/main/java/com/example/marinegame/play/GameActivity.kt
+++ app/src/main/java/com/example/marinegame/play/GameActivity.kt
@@ -2,59 +2,45 @@ package com.example.marinegame.play
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import com.example.marinegame.R
+import com.example.marinegame.model.Game
+import com.example.marinegame.model.Word
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity(), GameContract.MvpView {
 
-    lateinit var words : List<Word>
     lateinit var randomWord : TextView
+    lateinit var presenter : GameContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-
-        randomWord = findViewById<TextView>(R.id.random_word_textview)
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://bridge.buddyweb.fr/api/wordgeneration/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val generateWordApi = retrofit.create(GenerateWordApi::class.java)
-        val call : Call<List<Word>> =  generateWordApi.getWords()
-
-        call.enqueue(object : Callback<List<Word>> {
-            override fun onResponse(call: Call<List<Word>>, response: Response<List<Word>>) {
-
-                if (!response.isSuccessful()) {
-                    randomWord.text = "Aucun mot disponible. Vérifiez votre connexion"
-                    return
-                }
-
-                words = response.body()!!
-
-            }
-
-            override fun onFailure(call: Call<List<Word>>, t: Throwable) {
-                randomWord.text = t.message
-            }
-        })
-
+        randomWord = findViewById(R.id.random_word_textview)
+        presenter = GamePresenter(this, GetWordIntractorImpl())
     }
 
     fun backToHome(view: View) {
         finish()
     }
 
-    fun generateWord(view: View) {
-        randomWord.text = Game.getRandomWord(words)
+    fun onClickScreen(view: View) {
+        presenter.onRefreshClick()
+    }
+
+    override fun onResponseFailure(throwable: Throwable) {
+        Toast.makeText(this,
+            "Oups, une erreur est survenue commandant",
+            Toast.LENGTH_LONG).show();
+    }
+
+    override fun generateWord(word: String) {
+        randomWord.text = word
     }
 }
