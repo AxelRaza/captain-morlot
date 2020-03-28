0c0eb27306e34bafd910a5be5ae4670e2fc1363d
Axel.Razafimahefa <axel.raza75@hotmail.fr>
2020-03-28 16:56:32 +0100

    move logic to game

--- app/src/main/java/com/example/marinegame/model/Player.kt
+++ app/src/main/java/com/example/marinegame/model/Player.kt
@@ -2,5 +2,4 @@ package com.example.marinegame.model
 
 import java.io.Serializable
 
-class Player(val name: String, val role: Role) : Serializable {
-}
+class Player(val name: String, var role: String) : Serializable {}
--- app/src/main/java/com/example/marinegame/model/Role.kt
+++
@@ -1,6 +0,0 @@
-package com.example.marinegame.model
-
-import java.io.Serializable
-
-class Role(var name: String, var description: String) : Serializable {
-}
